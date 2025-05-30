49389c2a72ee39d5d1ea87dabcea183e1ef39c8e
fransoufil <fran.sf@uol.com.br>
2025-05-30 20:43:54 -0300

    IAuditoriaService

---
+++ src/main/java/com/fransoufil/webflux_mini_demo/service/IAuditoriaService.java
@@ -0,0 +1,11 @@
+package com.fransoufil.webflux_mini_demo.service;
+
+import com.fransoufil.webflux_mini_demo.model.LogAcesso;
+import com.fransoufil.webflux_mini_demo.model.Usuario;
+import reactor.core.publisher.Mono;
+
+public interface IAuditoriaService {
+
+    public Mono<LogAcesso> registrarAcesso(Usuario usuario);
+
+}
